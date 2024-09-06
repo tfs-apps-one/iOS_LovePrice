3daac440e60fbf904d4a6475a6f382093c7dd015
tfs-apps-one <furu12080205@gmail.com>
2024-09-07 08:32:28 +0900

    Initial Commit

---
+++ LovePrice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LovePrice
+//
+//  Created by 古川貴史 on 2024/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LovePrice/LovePriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovePriceApp.swift
+//  LovePrice
+//
+//  Created by 古川貴史 on 2024/09/07.
+//
+
+import SwiftUI
+
+@main
+struct LovePriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
